3fba74d7fe136ba3b485b7051f5e5b49f06898e6
haekyu31 <109947712+haekyu31@users.noreply.github.com>
2022-08-04 14:51:01 +0900

    Time: 38 ms (87.35%), Space: 14 MB (10.84%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.py
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.py
@@ -1,11 +1,21 @@
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
         
-        i_list = [i for i, nums in enumerate(nums)]
-        j_list = [j for j, nums in enumerate(nums)]
-        count = 0
-        for i in i_list:
-            for j in j_list:
-                if nums[i] == nums[j] and i < j:
-                    count += 1
-        return count        
+        # i_list = [i for i, nums in enumerate(nums)]
+        # j_list = [j for j, nums in enumerate(nums)]
+        # count = 0
+        # for i in i_list:
+        #     for j in j_list:
+        #         if nums[i] == nums[j] and i < j:
+        #             count += 1
+        # return count        
+        
+        
+                # 모든 digit에 대한 count를 구한다.
+        Counts = collections.Counter(nums)
+        # count가 2개 이상인 경우에 대해서 nC2 (pair의 수)를 Answer에 더한다
+        Answer = 0
+        for key, value in Counts.items():
+            if value > 1:
+                Answer += value * (value-1) / 2 # nC2
+        return int(Answer)
