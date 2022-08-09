f03ed4ece739b1d55ea05c4c6d38239de17814ef
haekyu31 <109947712+haekyu31@users.noreply.github.com>
2022-08-09 10:13:33 +0900

    Time: 54 ms (29.23%), Space: 13.8 MB (96.51%) - LeetHub

--- 2160-minimum-sum-of-four-digit-number-after-splitting-digits/2160-minimum-sum-of-four-digit-number-after-splitting-digits.py
+++ 2160-minimum-sum-of-four-digit-number-after-splitting-digits/2160-minimum-sum-of-four-digit-number-after-splitting-digits.py
@@ -7,3 +7,22 @@ class Solution:
         digits_10 = sum(numlist[0:2])*10
         digits_1 = sum(numlist[2:])
         return digits_10 + digits_1
+    
+    
+    
+    
+    # class Solution:
+    # def minimumSum(self, num: int) -> int:
+    #     numList = [ int(i) for i in list(str(num))]
+    #     print(numList)
+    #     # Greedy algorithm
+    #     numList.sort()
+    #     SecondDigit1 = numList[0]
+    #     SecondDigit2 = numList[1]
+    #     #10의자리수
+    #     SecondDigit = (SecondDigit1 + SecondDigit2) * 10
+    #     FirstDigit1 = numList[2]
+    #     FirstDigit2 = numList[3]
+    #     #1의자리수
+    #     FirstDigit = FirstDigit1 + FirstDigit2
+    #     return SecondDigit + FirstDigit 
