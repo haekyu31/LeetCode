dcf4637a3aee27c407778306a66d4ff056b94995
haekyu31 <109947712+haekyu31@users.noreply.github.com>
2022-08-09 10:12:30 +0900

    Time: 1772 ms (5.02%), Space: 20.3 MB (5.88%) - LeetHub

--- 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.py
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.py
@@ -12,3 +12,31 @@ class Solution:
                     current.append(0)   
             Answer.append(current)       # for loop 한바퀴를 돌았을때 Answer 리스트에 넣기
         return (len(Answer))
+    
+    
+    
+#     # 1689 기본풀이
+# class Solution:
+#     def minPartitions(self, n: str) -> int:
+#         nList = [int(i) for i in list(str(n))]
+#         return max(nList)
+# # 1689 Greedy 알고리즘 출력 풀이
+# class Solution:
+#     def minPartitions(self, n: str) -> int:
+#         InputList = [ int(i) for i in list(n) ]
+#         MinusList = []
+#         nCount = 0
+#         while sum(InputList) != 0:
+#             Minus = []
+#             for i, digit in enumerate(InputList):
+#                 if digit != 0:
+#                     Minus.append(1)
+#                     InputList[i] -= 1
+#                 elif digit == 0:
+#                     Minus.append(0)
+#             Minus = ''.join([str(i) for i in Minus])
+#             MinusList.append(Minus)
+#             nCount += 1
+#         print(MinusList)
+#         return nCount
+#         # return max([ int(i) for i in list(n)])
