f26bbbd60ab92a1b13f59ff4521f0b41e41a2b02
haekyu31 <109947712+haekyu31@users.noreply.github.com>
2022-08-16 15:39:10 +0900

    Time: 2590 ms (5.02%), Space: 25.1 MB (52.25%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.py
+++ 739-daily-temperatures/739-daily-temperatures.py
@@ -1,13 +1,41 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        WaitDay = [0 for i in range(len(temperatures))]
-        Stack = [[-1, temperatures[0]] ]
-        temperatures.pop(0)
-        for i, tem in enumerate(temperatures):
-        # if increasead temperature
-            while Stack and Stack[-1][1] < tem:
-                Last_idx, Last_tem = Stack.pop()
-                WaitDay[Last_idx + 1] = i - Last_idx
-            else:
-                Stack.append([i, tem])
-        return WaitDay
+        temp = temperatures
+        Day = [0]*len(temp)
+        Stack = []
+        for i, v in enumerate(temp):
+            while Stack and Stack[-1][1] < temp[i]:
+                idx, value = Stack.pop()
+                Day[idx] = i - idx
+            Stack.append([i,v])
+        return Day
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # WaitDay = [0 for i in range(len(temperatures))]
+        # Stack = [[-1, temperatures[0]] ]
+        # temperatures.pop(0)
+        # for i, tem in enumerate(temperatures):
+        # # if increasead temperature
+        #     while Stack and Stack[-1][1] < tem:
+        #         Last_idx, Last_tem = Stack.pop()
+        #         WaitDay[Last_idx + 1] = i - Last_idx
+        #         print(Stack, temperatures, Last_idx, WaitDay)
+        #     else:
+        #         Stack.append([i, tem])
+        #         print(Stack, temperatures, Last_idx, WaitDay)
+        # return WaitDay
