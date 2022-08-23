df391abfa5952ae484986c7d2b2b95f6b22d8428
haekyu31 <109947712+haekyu31@users.noreply.github.com>
2022-08-23 10:07:23 +0900

    Time: 33 ms (92.02%), Space: 13.8 MB (54.22%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -1,10 +1,15 @@
 class Solution:
     def fib(self, n: int) -> int:
-        if n ==0 :
-            return 0
-        if n == 1:
-            return 1
-        return self.fib(n-1)+self.fib(n-2)
+        li = [0,1]
+        while len(li)< n+1:
+            li.append(li[-1]+li[-2])
+        return li[n]
+        
+#         if n ==0 :
+#             return 0
+#         if n == 1:
+#             return 1
+#         return self.fib(n-1)+self.fib(n-2)
             
 #     def Recur(self,n):
 #         if n == 0: 
