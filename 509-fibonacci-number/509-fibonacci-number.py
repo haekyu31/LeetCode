6900dd94321631b23c88d0b2033a409c95b4e056
haekyu31 <109947712+haekyu31@users.noreply.github.com>
2022-08-23 10:01:06 +0900

    Time: 941 ms (19.57%), Space: 13.9 MB (9.62%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def Recur(self,n):
+        if n == 0: 
+            return 0
+        if n == 1:
+            return 1
+        return self.Recur(n-1)+ self.Recur(n-2)
+    
+    def fib(self, n: int) -> int:
+        Answer = self.Recur(n)
+        return Answer
+    
