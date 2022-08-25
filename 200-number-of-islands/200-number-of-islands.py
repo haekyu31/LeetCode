726dd0a44875e25a67ddfeac21b815c04e5efc3b
haekyu31 <109947712+haekyu31@users.noreply.github.com>
2022-08-25 14:38:57 +0900

    Time: 419 ms (69.55%), Space: 16.5 MB (65.51%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,11 +1,7 @@
 class Solution:
     def DFS(self, grid, idx_x, idx_y):
-        if idx_x <0 or idx_y <0 :
+        if idx_x <0 or idx_y <0 or idx_x >=self.x or idx_y >= self.y or grid[idx_y][idx_x] == "0" :
             return 
-        if idx_x >=self.x or idx_y >= self.y:
-            return
-        if grid[idx_y][idx_x] == "0" :
-            return
         grid[idx_y][idx_x] = "0"
         self.DFS(grid, idx_x, idx_y+1)
         self.DFS(grid, idx_x+1, idx_y)
@@ -14,8 +10,7 @@ class Solution:
         
     def numIslands(self, grid: List[List[str]]) -> int:
         self.Count = 0
-        self.y = len(grid)
-        self.x = len(grid[0])
+        self.y, self.x = len(grid), len(grid[0])
         for idx_x in range(self.x):
             for idx_y in range(self.y):
                 if grid[idx_y][idx_x] == "1":
