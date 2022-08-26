0fb74b03d504f92aa35a36723f702d0fbb7823e5
haekyu31 <109947712+haekyu31@users.noreply.github.com>
2022-08-26 11:08:24 +0900

    Time: 336 ms (10.69%), Space: 20 MB (39.53%) - LeetHub

---
+++ 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def DFS(self, Node, low = 10**5+1, high = -1):
+        if not Node:
+            self.Answer.append(abs(high - low))
+            return 
+        if Node.val < low:
+            low = Node.val
+        if Node.val > high:
+            high = Node.val
+        self.DFS(Node.left, low, high)
+        self.DFS(Node.right, low, high)
+        return max(self.Answer)
+    def maxAncestorDiff(self, root: Optional[TreeNode]) -> int:
+        self.Max, self.Min = -1, 200
+        self.Answer =[]
+        return self.DFS(root)
