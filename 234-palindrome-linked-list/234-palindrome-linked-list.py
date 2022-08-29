5098fc51ab42e7824689ae656cadb5d636b1d311
haekyu31 <109947712+haekyu31@users.noreply.github.com>
2022-08-29 17:19:14 +0900

    Time: 1446 ms (23.98%), Space: 46.6 MB (56.49%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.py
+++ 234-palindrome-linked-list/234-palindrome-linked-list.py
@@ -3,31 +3,31 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+# class Solution:
+#     def isPalindrome(self, head: Optional[ListNode]) -> bool:
+#         Node = head
+#         list_node = []
+#         while Node : 
+#             list_node.append(Node.val)
+#             Node = Node.next
+#         print(list_node)
+        
+#         #two point
+#         i,j  = 0, len(list_node)-1
+#         while i< j :
+#             if list_node[i] != list_node[j]:
+#                 return False
+#             else: 
+#                 i += 1
+#                 j -= 1
+#         return True
+    
+    
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        Node = head
-        list_node = []
-        while Node : 
-            list_node.append(Node.val)
-            Node = Node.next
-        print(list_node)
-        
-        #two point
-        i,j  = 0, len(list_node)-1
-        while i< j :
-            if list_node[i] != list_node[j]:
-                return False
-            else: 
-                i += 1
-                j -= 1
-        return True
-    
-    
-    # class Solution:
-    # def isPalindrome(self, head: Optional[ListNode]) -> bool:
-    #     PointerNode = head
-    #     myList = list()
-    #     while PointerNode:
-    #         myList.append(PointerNode.val)
-    #         PointerNode = PointerNode.next
-    #     return myList == list(reversed(myList))
+        PointerNode = head
+        myList = list()
+        while PointerNode:
+            myList.append(PointerNode.val)
+            PointerNode = PointerNode.next
+        return myList == list(reversed(myList))
