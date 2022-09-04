662e820763d440d4a0beb6013658029191150aa1
haekyu31 <109947712+haekyu31@users.noreply.github.com>
2022-09-04 23:39:31 +0900

    Time: 102 ms (19.15%), Space: 14.1 MB (24.68%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.py
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.py
@@ -12,10 +12,24 @@
 #                 else : 
 #                     j += 1
 #         return output
-class Solution:
-    def set_intersection(self, set1, set2):
-        return [x for x in set1 if x in set2]
+# class Solution:
+#     def set_intersection(self, set1, set2):
+#         return [x for x in set1 if x in set2]
         
+#     def intersection(self, nums1, nums2):
+#         """
+#         :type nums1: List[int]
+#         :type nums2: List[int]
+#         :rtype: List[int]
+#         """  
+#         set1 = set(nums1)
+#         set2 = set(nums2)
+        
+#         if len(set1) < len(set2):
+#             return self.set_intersection(set1, set2)
+#         else:
+#             return self.set_intersection(set2, set1)
+class Solution:
     def intersection(self, nums1, nums2):
         """
         :type nums1: List[int]
@@ -24,9 +38,4 @@ class Solution:
         """  
         set1 = set(nums1)
         set2 = set(nums2)
-        
-        if len(set1) < len(set2):
-            return self.set_intersection(set1, set2)
-        else:
-            return self.set_intersection(set2, set1)
-                
+        return list(set2 & set1)                
