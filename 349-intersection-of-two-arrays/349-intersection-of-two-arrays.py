9af8fa4e2bdcee3ae2c336ecebbd987b02ebb911
haekyu31 <109947712+haekyu31@users.noreply.github.com>
2022-09-04 23:39:06 +0900

    Time: 89 ms (32.57%), Space: 14.1 MB (24.68%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.py
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.py
@@ -1,16 +1,32 @@
-class Solution:
-    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        nums1= sorted(list(set(nums1)))
+# class Solution:
+#     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+#         nums1= sorted(list(set(nums1)))
         
-        output = []
-        for i in range(len(nums1)):
-            j = 0
-            while j < len(nums2):
-                if nums1[i] == nums2[j]:
-                    output.append(nums1[i])
-                    break
-                else : 
-                    j += 1
-        return output
-                    
+#         output = []
+#         for i in range(len(nums1)):
+#             j = 0
+#             while j < len(nums2):
+#                 if nums1[i] == nums2[j]:
+#                     output.append(nums1[i])
+#                     break
+#                 else : 
+#                     j += 1
+#         return output
+class Solution:
+    def set_intersection(self, set1, set2):
+        return [x for x in set1 if x in set2]
+        
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """  
+        set1 = set(nums1)
+        set2 = set(nums2)
+        
+        if len(set1) < len(set2):
+            return self.set_intersection(set1, set2)
+        else:
+            return self.set_intersection(set2, set1)
                 
