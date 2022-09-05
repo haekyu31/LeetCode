9d5a31b2ef3cd5e59b1c75126829fd222d60d48f
haekyu31 <109947712+haekyu31@users.noreply.github.com>
2022-09-05 13:49:09 +0900

    Time: 374 ms (42.43%), Space: 23.3 MB (8.97%) - LeetHub

--- 938-range-sum-of-bst/938-range-sum-of-bst.py
+++ 938-range-sum-of-bst/938-range-sum-of-bst.py
@@ -10,12 +10,12 @@ class Solution:
         node_sum = 0
         if not root:
             return
-        while queue:
-            value = queue.pop(0)
-            if low<= value.val <=high:
+        while queue:            # 모든 queue를 돌면 종료하도록 while을 사용한다.
+            value = queue.pop(0)  # 1개의 값을 먼저 계산하기 위해서 pop으로 꺼내온다.
+            if low<= value.val <=high:  #조건을 만족하면 저장한다.
                 node_sum += value.val
-            if value.left:
+            if value.left:              #다음 연결된 root가 있다면 이동할수 있도록 visited queue에 집어 넣는다.
                 queue.append(value.left)
-            if value.right:
+            if value.right:             #같은 높이의 root를 집어넣는다.
                 queue.append(value.right)
         return node_sum
