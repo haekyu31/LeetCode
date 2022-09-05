eb6a923934bfa0bab7178cbfae27b9ba6bfc4f95
haekyu31 <109947712+haekyu31@users.noreply.github.com>
2022-09-05 14:59:15 +0900

    Time: 59 ms (31.78%), Space: 14.2 MB (81.47%) - LeetHub

--- 404-sum-of-left-leaves/404-sum-of-left-leaves.py
+++ 404-sum-of-left-leaves/404-sum-of-left-leaves.py
@@ -4,20 +4,32 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+# class Solution:
+    # def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
+    #     # print(f'root: {root}')
+    #     queue = [root]
+    #     node_sum = 0
+    #     if not root:
+    #         return 
+    #     while queue:
+    #         # print([Idx.val for Idx in queue])
+    #         value = queue.pop(0)
+    #         if value.left:
+    #             queue.append(value.left)
+    #             if not (value.left.left or value.left.right):
+    #                 node_sum += value.left.val
+    #         if value.right:
+    #             queue.append(value.right)
+    #     return node_sum
 class Solution:
     def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
-        # print(f'root: {root}')
-        queue = [root]
-        node_sum = 0
-        if not root:
-            return 
-        while queue:
-            # print([Idx.val for Idx in queue])
-            value = queue.pop(0)
-            if value.left:
-                queue.append(value.left)
-                if not (value.left.left or value.left.right):
-                    node_sum += value.left.val
-            if value.right:
-                queue.append(value.right)
-        return node_sum
+        q, ans = deque([(root, False)]), 0
+        while q:
+            cur, isLeft = q.popleft()
+            if not cur.left and not cur.right and isLeft:
+                ans = ans + cur.val
+            if cur.right:
+                q.append((cur.right, False))
+            if cur.left: 
+                q.append((cur.left, True))
+        return ans
