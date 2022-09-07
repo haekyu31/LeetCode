8acb7cc631fec457138ec007e31d23211e50a38f
haekyu31 <109947712+haekyu31@users.noreply.github.com>
2022-09-07 23:50:08 +0900

    Time: 163 ms (12.49%), Space: 18.8 MB (22.99%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -5,22 +5,36 @@
 #         self.left = None
 #         self.right = None
 
+# class Solution:
+#     def DFS(self, node):
+#         if not node:
+#             return 0    
+#         Descent = self.DFS(node.left) + self.DFS(node.right)
+#         if node.val == self.p:
+#             Descent = Descent + 1
+#         if node.val == self.q:
+#             Descent = Descent + 1
+#         if Descent == 2:
+#             if self.Answer == 0:
+#                 self.Answer = node
+#         return Descent
+#     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+#         self.Answer = 0
+#         self.p = p.val
+#         self.q = q.val
+#         self.DFS(root)
+#         return self.Answer
 class Solution:
-    def DFS(self, node):
-        if not node:
-            return 0    
-        Descent = self.DFS(node.left) + self.DFS(node.right)
-        if node.val == self.p:
-            Descent = Descent + 1
-        if node.val == self.q:
-            Descent = Descent + 1
-        if Descent == 2:
-            if self.Answer == 0:
-                self.Answer = node
-        return Descent
-    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        self.Answer = 0
-        self.p = p.val
-        self.q = q.val
-        self.DFS(root)
-        return self.Answer
+    # @param {TreeNode} root
+    # @param {TreeNode} p
+    # @param {TreeNode} q
+    # @return {TreeNode}
+    def lowestCommonAncestor(self, root, p, q):
+        if not root or not p or not q:
+            return None
+        if (max(p.val, q.val) < root.val):
+            return self.lowestCommonAncestor(root.left, p, q)
+        elif (min(p.val, q.val) > root.val):
+            return self.lowestCommonAncestor(root.right, p, q)
+        else:
+            return root
