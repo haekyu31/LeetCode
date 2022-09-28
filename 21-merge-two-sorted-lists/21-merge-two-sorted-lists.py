064d4a6e0bf0d5ff2905e62b96bcff17d8f66201
haekyu31 <109947712+haekyu31@users.noreply.github.com>
2022-09-28 14:09:37 +0900

    Time: 55 ms (66.01%), Space: 13.9 MB (32.66%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
@@ -4,90 +4,23 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        
-#         root = Answer = ListNode()
-#         print(Answer)
-#         while list1 or list2 :
-#             if list1 and list2:
-#                 if list1.val < list2.val : 
-#                     value = list1.val
-#                     list1 = list1.next
-#                 else : 
-#                     value = list2.val
-#                     list2 = list2.next
-#             elif list1:
-#                 value = list1.val
-#                 list1 = list1.next
-#             elif list2:
-#                 value = list2.val
-#                 list2 = list2.next
-#             print(value)
-#             Answer.next = ListNode(value)
-#             Answer = Answer.next
-            
-#         return root.next
-        
-#     #새로운 Linked List에 추가한다
-#         Header = ListNode(0)
-#         Pointer = Header
-        
-#         while list1 and list2:
-#             if list1.val< list2.val:
-#                 Pointer.next = ListNode(list1.val)
-#                 Pointer = Pointer.next
-#                 list1 = list1.next
-#             else: 
-#                 Pointer.next = ListNode(list2.val)
-#                 Pointer = Pointer.next
-#                 list2 = list2.next
-        
-#         while list1:
-#             Pointer.next = ListNode(list1.val)
-#             Pointer = Pointer.next
-#             list1 = list1.next
-#         while list2:
-#             Pointer.next = ListNode(list2.val)
-#             Pointer = Pointer.next
-#             list2 = list2.next
-            
-#         return Header.next
-
-        Header = Pointer =ListNode(0)
-        
-        while list1 and list2:
-            if list1.val < list2.val:
-                Pointer.next = list1
-                Pointer = Pointer.next
+     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        Node = res = ListNode()
+        while list1 or list2 :
+            if list1 and list2:
+                if list1.val < list2.val : 
+                    value = list1.val
+                    list1 = list1.next
+                else : 
+                    value = list2.val
+                    list2 = list2.next
+            elif list1:
+                value = list1.val
                 list1 = list1.next
-            else:
-                Pointer.next = list2
-                Pointer = Pointer.next
+            elif list2:
+                value = list2.val
                 list2 = list2.next
-        if list1:
-            Pointer.next = list1
-        
-        if list2:
-            Pointer.next = list2
+            res.next = ListNode(value)
+            res = res.next
             
-        return Header.next
-    
-    # class Solution:
-    # def mergeTwoLists(self, list1, list2) -> Optional[ListNode]:
-    #     Header = Pointer = ListNode(0, None)
-    #     while list1 and list2:
-    #         if list1.val < list2.val:
-    #             Pointer.next = list1
-    #             Pointer = Pointer.next
-    #             list1 = list1.next
-    #         else:
-    #             Pointer.next = list2
-    #             Pointer = Pointer.next
-    #             list2 = list2.next
-    #     if list1:
-    #         Pointer.next = list1
-    #     if list2:
-    #         Pointer.next = list2
-    #     return Header.next
-
-                
+        return Node.next
