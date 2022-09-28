659c54bbe2936ce248da080ece76e6d19530dcb7
haekyu31 <109947712+haekyu31@users.noreply.github.com>
2022-09-29 02:06:38 +0900

    Time: 61 ms (18.91%), Space: 13.9 MB (74.76%) - LeetHub

--- 232-implement-queue-using-stacks/232-implement-queue-using-stacks.py
+++ 232-implement-queue-using-stacks/232-implement-queue-using-stacks.py
@@ -1,63 +1,20 @@
 class MyQueue:
+    def __init__(self):
+        self.s1 = []
+        self.s2 = []
 
-    def __init__(self): # 빈 리스트를 만들어서 큐 스택을 사용하기
-        self.Stack1 = []
-        self.Stack2 = []
+    def push(self, x):
+        while self.s1:
+            self.s2.append(self.s1.pop())
+        self.s1.append(x)
+        while self.s2:
+            self.s1.append(self.s2.pop())
 
-    def push(self, x: int) -> None:
-        self.Stack1.append(x)  #큐에 집어넣기
+    def pop(self):
+        return self.s1.pop()
 
-    def pop(self) -> int:
-        while len(self.Stack1)>0:
-            self.Stack2.append(self.Stack1.pop())
-        Answer = self.Stack2.pop()
-        while len(self.Stack2)>0:
-            self.Stack1.append(self.Stack2.pop())
-        return Answer
-        
-    def peek(self) -> int:
-        while len(self.Stack1)>0:
-            self.Stack2.append(self.Stack1.pop())
-        Answer = self.Stack2[-1]
-        while len(self.Stack2)>0:
-            self.Stack1.append(self.Stack2.pop())
-        return Answer
-        
+    def peek(self):
+        return self.s1[-1]
 
-    def empty(self) -> bool:
-        return len(self.Stack1)==0
-        
-
-
-# Your MyQueue object will be instantiated and called as such:
-# obj = MyQueue()
-# obj.push(x)
-# param_2 = obj.pop()
-# param_3 = obj.peek()
-# param_4 = obj.empty()
-
-
-
-# class MyQueue:
-#     def __init__(self):
-#         self.Stack1 = []
-#         self.Stack2 = []
-#     def push(self, x: int) -> None:
-#         self.Stack1.append(x)
-#     def pop(self) -> int:
-#         # Stack1에서 pop하여 Stack2에 쌓기 (뒤집어서 들어감)
-#         while len(self.Stack1) > 0:
-#             self.Stack2.append(self.Stack1.pop())
-#         returnValue = self.Stack2.pop()
-#         while len(self.Stack2) > 0:
-#             self.Stack1.append(self.Stack2.pop())
-#         return returnValue
-#     def peek(self) -> int:
-#         while len(self.Stack1) > 0:
-#             self.Stack2.append(self.Stack1.pop())
-#         returnValue = self.Stack2[ len(self.Stack2) -1 ]
-#         while len(self.Stack2) > 0:
-#             self.Stack1.append(self.Stack2.pop())
-#         return returnValue
-#     def empty(self) -> bool:
-#         return len(self.Stack1) == 0
+    def empty(self):
+        return not self.s1
