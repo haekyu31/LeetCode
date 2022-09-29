1dcfb81b0f7f62aed37728141e28d6ea3c8434f8
haekyu31 <109947712+haekyu31@users.noreply.github.com>
2022-09-29 15:15:36 +0900

    Time: 58 ms (36.84%), Space: 13.8 MB (72.33%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -1,23 +1,35 @@
 class Solution:
-    def isValid(self, s: str) -> bool:
-        stack = []
-        strings = list(s)
+#     def isValid(self, s: str) -> bool:
+#         stack = []
+#         strings = list(s)
         
-        for i in strings:
-            if i in ['(', '{', '[']:
-                stack.append(i)
+#         for i in strings:
+#             if i in ['(', '{', '[']:
+#                 stack.append(i)
+#             else:
+#                 if len(stack) ==0:
+#                     return False
+#                 if stack[-1] =='(' and i == ')':
+#                     stack.pop()
+#                 elif stack[-1] =='{' and i == '}':
+#                     stack.pop()
+#                 elif stack[-1] =='[' and i == ']':
+#                     stack.pop()
+#                 else:
+#                     return False
+#         if len(stack)>0:
+#             return False
+#         return True
+    
+    def isValid(self, s):
+        stack = []
+        dict = {"]":"[", "}":"{", ")":"("}
+        for char in s:
+            if char in dict.values():
+                stack.append(char)
+            elif char in dict.keys():
+                if stack == [] or dict[char] != stack.pop():
+                    return False
             else:
-                if len(stack) ==0:
-                    return False
-                if stack[-1] =='(' and i == ')':
-                    stack.pop()
-                elif stack[-1] =='{' and i == '}':
-                    stack.pop()
-                elif stack[-1] =='[' and i == ']':
-                    stack.pop()
-                else:
-                    return False
-                
-        if len(stack)>0:
-            return False
-        return True
+                return False
+        return stack == []
