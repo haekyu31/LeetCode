df9225db74698c23dd24d5509c053943c9e9091e
haekyu31 <109947712+haekyu31@users.noreply.github.com>
2022-10-20 09:32:56 +0900

    Time: 110 ms (29.49%), Space: 13.8 MB (80.12%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.py
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -1,9 +1,23 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        values = [ 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 ]
-        numerals = [ "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" ]
-        res = ""
-        for index, value in enumerate(values):
-            res += (num//value)*numerals[index]
-            num %= value
-        return res
+        roman = {
+        1000: 'M',
+        900: 'CM',
+        500: 'D',
+        400: 'CD',
+        100: 'C',
+        90: 'XC',
+        50: 'L',
+        40: 'XL',
+        10: 'X',
+        9: 'IX',
+        5: 'V',
+        4: 'IV',
+        1: 'I'
+        }
+        romanized = ''
+        for base, symb in roman.items():
+            romanized += symb *(num//base)
+            num %= base
+        else:
+            return romanized
