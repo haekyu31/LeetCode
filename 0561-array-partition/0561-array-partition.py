a2d9157adfc300405e0152cf3aa74e1525031331
haekyu31 <109947712+haekyu31@users.noreply.github.com>
2022-10-27 22:22:08 +0900

    Time: 306 ms (85.09%), Space: 16.9 MB (5.51%) - LeetHub

--- 0561-array-partition/0561-array-partition.py
+++ 0561-array-partition/0561-array-partition.py
@@ -1,16 +1,21 @@
 class Solution:
     def arrayPairSum(self, nums: List[int]) -> int:
-        K = 10000
-        element_to_count = [0]*(2*K+1)
-        for element in nums:
-            element_to_count[element+K] +=1
+#         K = 10000
+#         element_to_count = [0]*(2*K+1)
+#         for element in nums:
+#             element_to_count[element+K] +=1
             
+#         max_sum = 0
+#         is_even_index = True
+#         for element in range(2*K+1):
+#             while element_to_count[element] >0:
+#                 if is_even_index:
+#                     max_sum += element -K
+#                 is_even_index = not is_even_index;
+#                 element_to_count[element] -=1
+#         return max_sum
+        nums.sort()
         max_sum = 0
-        is_even_index = True
-        for element in range(2*K+1):
-            while element_to_count[element] >0:
-                if is_even_index:
-                    max_sum += element -K
-                is_even_index = not is_even_index;
-                element_to_count[element] -=1
+        for i in range(0, len(nums),2):
+            max_sum += nums[i]
         return max_sum
