08e6ae9be6897bcf81dfe5308492e1b2fdf4d990
haekyu31 <109947712+haekyu31@users.noreply.github.com>
2022-11-02 13:16:06 +0900

    Time: 167 ms (62.37%), Space: 14.5 MB (72.41%) - LeetHub

--- 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
@@ -1,13 +1,23 @@
 class Solution:
     def findItinerary(self, tickets: List[List[str]]) -> List[str]:
         graph = collections.defaultdict(list)
-        for a, b in sorted(tickets):
+        for a, b in sorted(tickets, reverse = True):
             graph[a].append(b)
         route = []
         def dfs(a):
             while graph[a]:
-                dfs(graph[a].pop(0))    # 이동결로가 존재한다면 어휘순으로 첫번째 이동경로를 가져온다
-            route.append(a)             # 더이상 이동할 곳이 없을 경우 역순으로 담기기 시작한다. 
+                dfs(graph[a].pop())
+            route.append(a)
         dfs('JFK')
         return route[::-1]
-    # 많이 어려운 문제
+    #     graph = collections.defaultdict(list)
+    #     for a, b in sorted(tickets):
+    #         graph[a].append(b)
+    #     route = []
+    #     def dfs(a):
+    #         while graph[a]:
+    #             dfs(graph[a].pop(0))    # 이동결로가 존재한다면 어휘순으로 첫번째 이동경로를 가져온다
+    #         route.append(a)             # 더이상 이동할 곳이 없을 경우 역순으로 담기기 시작한다. 
+    #     dfs('JFK')
+    #     return route[::-1]
+    # # 많이 어려운 문제
