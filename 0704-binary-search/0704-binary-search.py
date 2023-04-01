2f0918b5624e9999500d71a056868e939a8e18ff
haekyu31 <109947712+haekyu31@users.noreply.github.com>
2023-04-01 13:00:34 +0900

    Time: 249 ms (41.36%), Space: 15.4 MB (96.81%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -1,32 +1,43 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         left, right = 0, len(nums)-1
-        while left<= right:
-            mid = (left + right)//2
+        while left <= right :
+            mid = (left + right) //2
             
-            if nums[mid] <target:
-                left = mid+1
-            elif nums[mid] > target:
-                right = mid-1
+            if nums[mid] < target:
+                left = mid + 1
+            elif nums[mid] > target :
+                right = mid -1
             else:
                 return mid
         return -1
-        # index = bisect.bisect_left(nums, target)
-        # if index < len(nums) and nums[index] == target:
-        #     return index
-        # else:
-        #     return -1
-#         def binary_search(left, right):
-#             if left <=right:
-#                 mid = (left + right)//2
-                
-#                 if nums[mid]<target:
-#                     return binary_search(mid+1, right)
-#                 elif nums[mid]>target:
-#                     return binary_search(left, mid-1)
-#                 else:
-#                     return mid
-#             else:
-#                 return -1
+#         left, right = 0, len(nums)-1
+#         while left<= right:
+#             mid = (left + right)//2
             
-#         return binary_search(0, len(nums)-1)
+#             if nums[mid] <target:
+#                 left = mid+1
+#             elif nums[mid] > target:
+#                 right = mid-1
+#             else:
+#                 return mid
+#         return -1
+#         # index = bisect.bisect_left(nums, target)
+#         # if index < len(nums) and nums[index] == target:
+#         #     return index
+#         # else:
+#         #     return -1
+# #         def binary_search(left, right):
+# #             if left <=right:
+# #                 mid = (left + right)//2
+                
+# #                 if nums[mid]<target:
+# #                     return binary_search(mid+1, right)
+# #                 elif nums[mid]>target:
+# #                     return binary_search(left, mid-1)
+# #                 else:
+# #                     return mid
+# #             else:
+# #                 return -1
+            
+# #         return binary_search(0, len(nums)-1)
