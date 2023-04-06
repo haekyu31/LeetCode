fa625dde3078badfde5896380163e5e7538ce85f
haekyu31 <109947712+haekyu31@users.noreply.github.com>
2023-04-06 14:38:13 +0900

    Time: 37 ms (26.03%), Space: 13.8 MB (45.08%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.py
+++ 0771-jewels-and-stones/0771-jewels-and-stones.py
@@ -1,17 +1,23 @@
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
-        freqs = {}
+        freqs = collections.Counter(stones)
         count = 0
         
-        # 돌의 빈도 수 계산
-        for char in stones :
-            if char not in freqs:
-                freqs[char] = 1
-            else:
-                freqs[char] += 1
-                
-        # 보석의 빈도 수 합산
         for char in jewels:
-            if char in freqs:
-                count += freqs[char]
+            count += freqs[char]
         return count
+#         freqs = {}
+#         count = 0
+        
+#         # 돌의 빈도 수 계산
+#         for char in stones :
+#             if char not in freqs:
+#                 freqs[char] = 1
+#             else:
+#                 freqs[char] += 1
+                
+#         # 보석의 빈도 수 합산
+#         for char in jewels:
+#             if char in freqs:
+#                 count += freqs[char]
+#         return count
