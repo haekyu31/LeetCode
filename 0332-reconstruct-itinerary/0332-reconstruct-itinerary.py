15c6c3b1da25e8ec743d371ad80a0cf26a89d408
haekyu31 <109947712+haekyu31@users.noreply.github.com>
2023-04-12 16:58:37 +0900

    Time: 70 ms (97.91%), Space: 14.1 MB (99.30%) - LeetHub

--- 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
@@ -4,31 +4,40 @@ class Solution:
         for a, b in sorted(tickets):
             graph[a].append(b)
             
-        route, stack = [], ["JFK"]
+        route, stack = [], ['JFK']
+        
         while stack:
+            # 반복으로 스택을 구성하되 막히는 부분에서 풀어내는 처리
             while graph[stack[-1]]:
                 stack.append(graph[stack[-1]].pop(0))
             route.append(stack.pop())
             
         return route[::-1]
-        # graph = collections.defaultdict(list)
-        # for a, b in sorted(tickets, reverse = True):
-        #     graph[a].append(b)
-        # route = []
-        # def dfs(a):
-        #     while graph[a]:
-        #         dfs(graph[a].pop())
-        #     route.append(a)
-        # dfs('JFK')
-        # return route[::-1]
-    #     graph = collections.defaultdict(list)
-    #     for a, b in sorted(tickets):
-    #         graph[a].append(b)
-    #     route = []
-    #     def dfs(a):
-    #         while graph[a]:
-    #             dfs(graph[a].pop(0))    # 이동결로가 존재한다면 어휘순으로 첫번째 이동경로를 가져온다
-    #         route.append(a)             # 더이상 이동할 곳이 없을 경우 역순으로 담기기 시작한다. 
-    #     dfs('JFK')
-    #     return route[::-1]
-    # # 많이 어려운 문제
+#         # 그래프를 뒤집어서 구성
+#         for a, b in sorted(tickets, reverse = True):
+#             graph[a].append(b)
+#         route = []
+        
+#         def dfs(a):
+#             while graph[a]:
+#                 dfs(graph[a].pop())
+#             route.append(a)
+        
+#         dfs('JFK')
+#         return route[::-1]
+#         # 그래프 순서대로 구성
+#         for a, b in sorted(tickets):
+#             graph[a].append(b)
+
+#         route = []
+        
+#         def dfs(a):
+#             # 첫 번째 값을 읽어 어휘 순 방문
+#             while graph[a]:
+#                 dfs(graph[a].pop(0))
+#             route.append(a)
+        
+#         dfs('JFK')
+#         # 다시 뒤집어 어휘 순 결과로 
+#         return route[::-1]
+            
