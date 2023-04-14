23b6f32e7939a14c8684cdd6a014fd61501c77c0
haekyu31 <109947712+haekyu31@users.noreply.github.com>
2023-04-14 13:30:35 +0900

    Time: 496 ms (26.33%), Space: 16.8 MB (21.70%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.py
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -1,11 +1,13 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        # 다익스트라 알고리즘
+        # 최단경로 찾기 다익스트라 알고리즘
+        # 모든 노드가 신호를 받는데 걸리는 시간
+        # 모든 노드에 도달할 수 있는지 여부
         graph = collections.defaultdict(list)
         # 그래프 인접 리스트 구성
-        for u, v, w, in times:
+        for u, v, w in times:
             graph[u].append((v,w))
-        
+            
         # 큐 변수: [(소요시간, 정점)]
         Q = [(0,k)]
         dist = collections.defaultdict(int)
@@ -17,10 +19,8 @@ class Solution:
                 dist[node] = time
                 for v, w in graph[node]:
                     alt = time + w
-                    heapq.heappush(Q, (alt,v))
-                    
+                    heapq.heappush(Q, (alt, v))
         # 모든 노드의 최단 경로 존재 여부 판별
         if len(dist) == n:
             return max(dist.values())
         return -1
-        
