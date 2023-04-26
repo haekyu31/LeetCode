5eef16815ad7bfccb93cfa05cab8ffb75e356f43
haekyu31 <109947712+haekyu31@users.noreply.github.com>
2023-04-26 13:41:26 +0900

    Time: 1007 ms (99.83%), Space: 22 MB (5.00%) - LeetHub

--- 0134-gas-station/0134-gas-station.py
+++ 0134-gas-station/0134-gas-station.py
@@ -2,25 +2,23 @@ class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
         if sum(gas) < sum(cost):
             return -1
-        start, fuel = 0,0
-        
+        start, fuel = 0, 0
         for i in range(len(gas)):
-            #출발점이 안 되는 지점 판별
             if gas[i] + fuel < cost[i]:
-                start = i+1
+                start = i + 1
                 fuel = 0
             else:
-                fuel += gas[i] - cost[i]
+                fuel += gas[i] -cost[i]
         return start
-    
+        
 #         for start in range(len(gas)):
 #             fuel = 0
-#             for i in range(start, len(gas)+start):
+#             for i in range(start, len(gas) + start):
 #                 index = i % len(gas)
                 
 #                 can_travel = True
 #                 if gas[index] + fuel < cost[index]:
-#                     can_travel =False
+#                     can_travel = False
 #                     break
 #                 else:
 #                     fuel += gas[index] - cost[index]
