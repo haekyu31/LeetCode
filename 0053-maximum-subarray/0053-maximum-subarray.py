5bf27a72c793a9f72b93f6e575c74fe1e8136adf
haekyu31 <109947712+haekyu31@users.noreply.github.com>
2023-04-26 14:01:02 +0900

    Time: 755 ms (50.87%), Space: 28.7 MB (44.34%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,18 +1,5 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        # 카데인 알고리즘 
-        best_sum = -sys.maxsize
-        current_sum = 0
-        for num in nums:
-            current_sum = max(num, current_sum + num)
-            best_sum = max(best_sum, current_sum)
-        return best_sum
-    
-        # 앞에서부터 계속 값을 계산하면서 누적합을 계산
-        # 이전값이 0 보다 작은경우는 최댓값과 관계 없으므로 버린다.
-        # for i in range(1, len(nums)):
-        #     nums[i] += nums[i-1] if nums[i-1] >0 else 0
-        # return max(nums)
-    
-        
-        
+        for i in range(1, len(nums)):
+            nums[i] += nums[i -1] if nums[i-1] > 0 else 0
+        return max(nums)
