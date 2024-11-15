541aef8fbde9731015d44029d8142f98a29d7356
haekyu31 <109947712+haekyu31@users.noreply.github.com>
2024-11-15 22:26:28 +0900

    Time: 0 ms (100.00%), Space: 17.1 MB (65.35%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -0,0 +1,20 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        # 이분 탐색을 활용 전체 matrix는 오름차순이니까 
+        if not matrix:
+            return False
+        m, n = len(matrix), len(matrix[0])
+        left, right = 0, m * n - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+            mid_row, mid_col = divmod(mid, n)
+
+            if matrix[mid_row][mid_col] == target:
+                return True
+            elif matrix[mid_row][mid_col] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return False
